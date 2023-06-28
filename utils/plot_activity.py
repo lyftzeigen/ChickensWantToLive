a3fa47a5e24985abcacdf8c63e4d6caff179240b
Pavel Vasiliev <lyftzeigen@mail.ru>
2023-06-28 16:06:44 +0300

    Add activity plot

---
+++ utils/plot_activity.py
@@ -0,0 +1,133 @@
+import os
+import numpy
+import skimage
+import datetime
+import argparse
+import matplotlib.pyplot
+
+
+# Парсер аргументов командной строки
+parser = argparse.ArgumentParser(description="🐓 ActivityPlot")
+
+# Добавляем аргумент директории
+parser.add_argument(
+    "directory",
+    type=str,
+    help="Директория с детекциями/{directory}",
+)
+
+# Получаем аргументы
+args = parser.parse_args()
+
+print("🐓 ActivityPlot\n")
+
+# Создадим директории, если их нет
+if not os.path.isdir(os.path.join("processing", args.directory)):
+    os.makedirs(os.path.join("processing", args.directory))
+
+print(f"🚀 Чтение даных 'procerssing/{args.directory}'...\n")
+
+# Файл с детекциями
+filename = os.path.join("processing", args.directory, "detections.txt")
+
+# Чтение данных
+data = []
+
+with open(filename, "r") as file:
+    data = [int(row) for row in file.readlines()]
+
+# Параметры обработки данных (временное окно и частота кадров видео)
+time_interval = 30
+frames_per_second = 30
+
+# Разделяем данные на порции согласно временному окну
+split_data = numpy.array_split(data, len(data) // (frames_per_second * time_interval))
+
+# Реальное время начала съемки
+start_date_time = datetime.datetime(2023, 6, 9, 0, 1, 0)
+
+# Временные интервалы для отображения на графике
+split_data_timestamps = [
+    start_date_time + datetime.timedelta(seconds=td * time_interval)
+    for td in range(len(split_data))
+]
+
+# Характеристики активности для каждой порции данных
+split_data_median = numpy.array([numpy.median(batch) for batch in split_data]) * 1.0
+split_data_median_plus = numpy.array([numpy.max(batch) for batch in split_data]) * 1.0
+split_data_median_minus = numpy.array([numpy.min(batch) for batch in split_data]) * 1.0
+
+# Сглаживаем данные для презентативного вида
+split_data_median = skimage.filters.gaussian(split_data_median, 2)
+split_data_median_plus = skimage.filters.gaussian(split_data_median_plus, 2)
+split_data_median_minus = skimage.filters.gaussian(split_data_median_minus, 2)
+
+# Расчет временных параметров
+frames = len(data)
+seconds = frames // 30
+hours = seconds // 3600
+minutes = (seconds % 3600) // 60
+seconds = (seconds % 3600) % 60
+
+fig, ax = matplotlib.pyplot.subplots(dpi=300, figsize=(23, 5))
+
+ax.set_title(f"Активность птицы за {hours:02d}:{minutes:02d}:{seconds:02d}")
+ax.set_ylabel("Активность")
+ax.set_xlabel("Время")
+
+ax.plot(
+    split_data_timestamps,
+    split_data_median,
+    color="green",
+)
+
+ax.plot(
+    split_data_timestamps,
+    split_data_median_plus,
+    color="red",
+    alpha=0.25,
+    linestyle="--",
+)
+
+ax.plot(
+    split_data_timestamps,
+    split_data_median_minus,
+    color="blue",
+    alpha=0.25,
+    linestyle="--",
+)
+
+ax.fill_between(
+    split_data_timestamps,
+    split_data_median_plus,
+    split_data_median_minus,
+    color="blue",
+    alpha=0.1,
+)
+
+ax.set_xlim([split_data_timestamps[0], split_data_timestamps[-1]])
+
+ax.grid(linestyle=":")
+
+ax.legend(
+    [
+        "Медиана активности",
+        "Максимальная активность",
+        "Минимальная активность",
+    ],
+    loc="upper right",
+)
+
+ax.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(1 / 24 / 2))
+ax.xaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(1 / 24 / 12))
+ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%H:%M"))
+
+matplotlib.pyplot.gcf().autofmt_xdate()
+
+# Файл с графиком активности
+filename = os.path.join("processing", args.directory, "activity.png")
+
+# Сохраняем график
+fig.savefig(filename)
+
+print(f"\n🥵 Готово! Активность курочек можно посмотреть в файле '{filename}'")
